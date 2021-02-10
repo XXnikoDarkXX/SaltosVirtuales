f44ae81fcdedee1bb4f6599f47b666024f60b141
XXnikoDarkXX <nicocheto1@gmail.com>
2021-02-10 18:40:54 +0100

    Creacion del proyecto
    He creado el proyecto antes de hacer nada lo voy a subir vacio

---
+++ SaltosVirtuales/core/src/com/saltosvirtuales/Principal.java
@@ -0,0 +1,25 @@
+package com.saltosvirtuales;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Principal extends ApplicationAdapter {
+
+	@Override
+	public void create() {
+		super.create();
+
+
+	}
+
+	/**
+	 * Sirve para actualizar nuestro juego, como actualizar las posiciones ect...
+	 */
+	@Override
+	public void render() {
+		super.render();
+	}
+}
